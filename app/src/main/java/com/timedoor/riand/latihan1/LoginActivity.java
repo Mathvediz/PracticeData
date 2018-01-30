97412c3c919fd35efd7d27ab4fce0639302de6ca
Mathvediz <32832431+Mathvediz@users.noreply.github.com>
2018-01-30 15:27:09 +0800

    Initial commit

---
+++ app/src/main/java/com/timedoor/riand/latihan1/LoginActivity.java
@@ -0,0 +1,70 @@
+package com.timedoor.riand.latihan1;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.timedoor.riand.latihan1.Model.User;
+
+public class LoginActivity extends AppCompatActivity {
+    Button btnLogin;
+    EditText editPhone, editPassword;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        editPhone = (EditText) findViewById(R.id.phone);
+        editPassword = (EditText) findViewById(R.id.password);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //database init
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                final DatabaseReference table_user = database.getReference("user");
+
+                btnLogin.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        final ProgressDialog mDialog = new ProgressDialog(LoginActivity.this);
+                        mDialog.setMessage("Please Wait...");
+                        mDialog.show();
+                        table_user.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                if (dataSnapshot.child(editPhone.getText().toString()).exists()) {
+                                    mDialog.dismiss();
+                                    User user = dataSnapshot.child(editPhone.getText().toString()).getValue(User.class);
+                                    if (user.getPassword().equals(editPassword.getText().toString())) {
+                                        Toast.makeText(LoginActivity.this, "Sign in succesfully!", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Toast.makeText(LoginActivity.this, "Wrong Username or Password!", Toast.LENGTH_SHORT).show();
+                                    }
+                                } else {
+                                    mDialog.dismiss();
+                                    Toast.makeText(LoginActivity.this, "User not Found", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/timedoor/riand/latihan1/MainActivity.java
@@ -0,0 +1,34 @@
+package com.timedoor.riand.latihan1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnSignIn, btnSignUp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignIn = (Button) findViewById(R.id.btnSignIn);
+        btnSignUp = (Button) findViewById(R.id.btnSignUp);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent login = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity (login);
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
